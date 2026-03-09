e58141198c4ccc782d183f787fe0703755ae238f
Jujin <jujin@jujinkim.com>
2026-03-10 02:00:14 +0900

    Render community page dynamically

--- app/community/page.tsx
+++ app/community/page.tsx
@@ -1,6 +1,8 @@
 import Link from "next/link";
 import { getServiceSupabaseClient } from "@/lib/supabase/service";
 
+export const dynamic = "force-dynamic";
+
 export default async function CommunityHomePage() {
   const service = getServiceSupabaseClient();
 
